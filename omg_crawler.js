c132033e4481301abba2be3f6f4c836870f8b835
haihoang202 <haihoang202@yahoo.com>
2016-10-26 06:08:18 -0500

    2nd commit

--- omg_crawler.js
+++ omg_crawler.js
@@ -1,3 +1,4 @@
+//Import stuffs, prepare views, etc
 var express = require('express');
 
 var request = require('request');
@@ -22,14 +23,18 @@ app.set('port',port);
 
 app.use(express.static(__dirname + '/public'));
 
+//Entry point
 app.get('/', function(req, res) {
 	res.render('index');
 
 });
 
+var hashmap = {};
+
 app.post('/crawl', function(req, res){
 	var url = req.body.url;
-	//res.send(url);
+	
+	//Check if given url is valid
 	request(url, function(error, response, html){
 
 		if(error) {
@@ -37,16 +42,57 @@ app.post('/crawl', function(req, res){
 }
 		console.log("Status code: "+response.statusCode);
 		if(response.statusCode == 200){
+			//Load web content
 			var $ = cheerio.load(html);
+
+			//Create array to store discovered content
+			var anchors = [];
+			var images = [];
+			var scripts = [];
 			var links = [];
+			
+			
+			//Get anchor
 			$('a').each(function(i, elem) {
 				console.log($(this).attr('href'));
+				anchors[i] = $(this).attr('href');
+				hashmap["a "+anchors[i]] = 1;
+});
+			//Get images
+			$('img').each(function(i, elem) {
+				console.log($(this).attr('src'));
+				images[i] = $(this).attr('src');
+				hashmap["i "+images[i]] = 1;
+});
+
+			//Get scripts
+			$('script').each(function(i, elem) {
+				console.log($(this).attr('src'));
+				scripts[i] = $(this).attr('src');
+				hashmap["s "+scripts[i]] = 1;
+});
+
+			//Get links
+			$('link').each(function(i, elem) {
+				console.log($(this).attr('href'));
 				links[i] = $(this).attr('href');
+				hashmap["l "+links[i]] = 1;
 });
 			var text ='';
-			for(x in links){
-				text += links[x] + '<br>';
+
+			for(x in anchors){
+				text += 'Anchor: ' + anchors[x] + '<br>';
 }
+			for(x in images){
+				text += 'Image: ' + images[x] + '<br>';
+}	
+			for(x in scripts){
+				text += 'Script: ' + scripts[x] + '<br>';
+}	
+			for(x in links){
+				text += 'Link: ' + links[x] + '<br>';
+}
+			text += "Hashmap of entire sites: " + hashmap;
 			res.send(text);		
 }
 });
